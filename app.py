8d3a349fbfb84f66f2ab613c5ad672c839fb357d
chenx <chenx@yunsilicon.com>
2023-03-16 23:14:56 +0800

    Work with mysql

--- app.py
+++ app.py
@@ -1,40 +1,63 @@
-import os
-
-from flask import Flask, render_template, jsonify
-import xlrd
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:123456@localhost/test'
+db = SQLAlchemy(app)
 
-filename = 'data.xlsx'
-
-
-def read_file(file):
-    type = os.path.splitext(file)[1]
-    xy = []
-    if type == '.xlsx':
-        data = xlrd.open_workbook(filename)
-        table = data.sheet_by_index(0)
-        for i in range(0, table.ncols):
-            temp = {}
-            temp[table.col_values(i)[0]] = table.col_values(i)[1:]
-            xy.append(temp)
-    elif type == 'csv':
-        pass
-    elif type == 'txt':
-        pass
-    return xy
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(50))
 
 
-@app.route('/get_data')
-def get_data():
-    xy = read_file(filename)
-    return jsonify(xy)
-
-
+# define a route
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def index():
+  return 'Hello World'
+  
+# Create a new user
+@app.route('/user/<name>')
+def create_user(name):  
+  # execute a SQL query
+  user = User(name=name,email=f'{name}@163.com')
+  print(user)
+  db.session.add(user)
+  db.session.commit()
+  return 'User created'
+  
+# @app.route('/user/<name>')  
+# def create_user(name):
+#     # execute a SQL query
+#   # user = User(name="John", email="john@example.com")
+#   user = User(name=name, email=f'{name}@163.com')
+#   print(user)
+#   db.session.add(user)
+#   db.session.commit()
+  
+  # users = User.query.all()
+  # user = User.query.filter_by(name="John").first()
+  # user.email = "new-email@example.com"
+  # db.session.commit()
+  
+  # user = User.query.filter_by(name="John").first()
+  # user.email = "new-email@example.com"
+  # db.session.commit()
+  
+  
+  # user = User.query.filter_by(name="John").first()
+  # db.session.delete(user)
+  # db.session.commit()
+  
+@app.route('/user')
+def create_user_table():
+  # if user table not exits, create it accroding to class User
+  db.create_all()
+  return 'User table created'
+
+  
 
 
+# run the app
 if __name__ == '__main__':
-    app.run()
+  app.run(debug=True)
