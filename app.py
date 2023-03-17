50a20e499de34c5d6d3613e84457c0c59882b485
chenx <chenx@yunsilicon.com>
2023-03-17 11:08:39 +0800

    deal with user table

--- app.py
+++ app.py
@@ -25,31 +25,33 @@ def create_user(name):
   db.session.add(user)
   db.session.commit()
   return 'User created'
+
+# Get all users
+@app.route('/users')
+def get_users():
+  users = User.query.all()
+  output = []
+  for user in users:
+    user_data = {'name': user.name, 'email': user.email}
+    output.append(user_data)
+  return {'users': output}
+
   
-# @app.route('/user/<name>')  
-# def create_user(name):
-#     # execute a SQL query
-#   # user = User(name="John", email="john@example.com")
-#   user = User(name=name, email=f'{name}@163.com')
-#   print(user)
-#   db.session.add(user)
-#   db.session.commit()
-  
-  # users = User.query.all()
-  # user = User.query.filter_by(name="John").first()
-  # user.email = "new-email@example.com"
-  # db.session.commit()
-  
-  # user = User.query.filter_by(name="John").first()
-  # user.email = "new-email@example.com"
-  # db.session.commit()
-  
+# filter a user by name
+@app.route('/get_user/<name>')
+def get_user(name):
+  user = User.query.filter_by(name=name).first()
+  if not user:
+    return {'message': 'No user found!'}
+  user_data = {'name': user.name, 'email': user.email}
+  return {'user': user_data}
+
   
   # user = User.query.filter_by(name="John").first()
   # db.session.delete(user)
   # db.session.commit()
   
-@app.route('/user')
+@app.route('/create_table')
 def create_user_table():
   # if user table not exits, create it accroding to class User
   db.create_all()
